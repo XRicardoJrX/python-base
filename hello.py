1d6bee1b1584aaaa9c72ad2df7e6d77ac826a787
XRicardoJrX <ricardomorijunior703@gmail.com>
2024-06-06 13:00:13 +0000

    second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
+"""Hellp World Multi Linguas.
 
-#Este programa imprime Hello Word
+Dependendo da lingua configurada no ambiente, o programa exibe a mensagem correspondente.
 
-print ('Hello, World!')
-print (512+25245-1212-23)
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__="0.0.1"
+__author__="Ricardo Jr"
+__license__="Unlicense"
+
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+msg = "Hello, Wordl!"
+
+
+if current_language == "pt_BR.":
+    msg= "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg ="Ciao, Mondo"
+
+print (msg)
