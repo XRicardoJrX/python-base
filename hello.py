a1637e5f4b559adf007ef0132811afc7f3173366
XRicardoJrX <ricardomorijunior703@gmail.com>
2024-06-06 18:46:19 +0000

    venv

--- hello.py
+++ hello.py
@@ -18,11 +18,10 @@ Execução:
 __version__="0.0.1"
 __author__="Ricardo Jr"
 __license__="Unlicense"
-
 import os
 
-current_language = os.getenv("LANG")[:5]
-msg = "Hello, Wordl!"
+current_language = os.getenv("LANG","en_US")[:5]
+msg = "Hello, World!"
 
 
 if current_language == "pt_BR":
